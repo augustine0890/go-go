613af1cadc73905b07e807a5ec1c31061a58353b
augustine <augustino0890@gmail.com>
2021-01-10 17:37:37 +0900

    Go: Methods Struct

--- Learn-Go/structs/structs.go
+++ Learn-Go/structs/structs.go
@@ -13,6 +13,39 @@ func newPerson(name string) *person {
 	return &p
 }
 
+// Student methods defined on struct types
+type Student struct {
+	name   string
+	grades []int
+	age    int
+}
+
+func (s Student) getAge() int {
+	return s.age
+}
+
+func (s *Student) setAge(age int) {
+	s.age = age
+}
+
+func (s Student) getAverageGrade() float32 {
+	sum := 0
+	for _, v := range s.grades {
+		sum += v
+	}
+	return float32(sum) / float32(len(s.grades))
+}
+
+func (s *Student) getMaxGrade() int {
+	curMax := 0
+	for _, v := range s.grades {
+		if curMax < v {
+			curMax = v
+		}
+	}
+	return curMax
+}
+
 func main() {
 	fmt.Println(person{"Bob", 30})
 	fmt.Println(person{name: "Augustine", age: 31})
@@ -32,5 +65,19 @@ func main() {
 	sp.age = 51
 	fmt.Println(sp.age)
 	fmt.Println(s.age)
+
+	s1 := Student{name: "Augustine", grades: []int{80, 78, 92, 95}, age: 21}
+	s2 := Student{name: "John", grades: []int{80, 79, 78, 92, 99, 95}, age: 23}
+	fmt.Println("Student:", s1)
+	fmt.Println("Age Before:", s1.getAge())
+	s1.setAge(22)
+	fmt.Println("Age After:", s1.age)
+
+	average1 := s1.getAverageGrade()
+	fmt.Println("Average Grade:", average1)
+	fmt.Println("Max Grade:", s1.getMaxGrade())
+	average2 := s2.getAverageGrade()
+	fmt.Println("Average Grade:", average2)
+	fmt.Println("Max Grade:", s2.getMaxGrade())
 
 }
