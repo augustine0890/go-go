24fb4d3ac607132212f1be8f31c4c416461c05e8
augustine <augustino0890@gmail.com>
2021-02-12 23:18:28 +0900

    realworld app with golang

---
+++ Projects/realworld-app/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"runtime"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	// Start the default gin server
+	r := gin.Default()
+
+	// Load the .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file. Please create one in the root directory")
+	}
+
+	r.LoadHTMLGlob("./public/html/*")
+
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"realworldVersion": "v1.01",
+			"goVersion":        runtime.Version(),
+		})
+	})
+
+	port := os.Getenv("PORT")
+
+	r.Run(":" + port)
+}
