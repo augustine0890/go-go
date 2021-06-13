4367d89e84bce3713676a3f58f4ec21e0dabea07
augustine <augustino0890@gmail.com>
2021-06-13 23:55:46 +0900

    library: database config

--- clean-architecture/README.md
+++ clean-architecture/README.md
@@ -4,3 +4,8 @@
 - `go mod init library`
 - Install Gin package 
     - `go get -u github.com/gin-gonic/gin`
+- For loading `.env` file
+    - `go get -u github.com/joho/godotenv`
+- ORM library
+    - `go get -u gorm.io/gorm`
+    - `go get -g gorm.io/driver/postgres` #postgresql
---
+++ clean-architecture/library/config/database.go
@@ -0,0 +1,27 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	"github.com/joho/godotenv"
+	"gorm.io/gorm"
+)
+
+// Create a new connection to database
+func DatabaseConnection() *gorm.DB {
+	errEnv := godotenv.Load()
+	if errEnv != nil {
+		panic("Failed to load env file")
+	}
+
+	dbUser := os.Getenv("DATABASE_USER")
+	dbPass := os.Getenv("DATABASE_PASSWORD")
+	dbHost := os.Getenv("DATABASE_HOST")
+	dbName := os.Getenv("DATABASE_NAME")
+
+	dsn := "host=localhost user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Asia/Shanghai"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?charset=utf8&parseTime=True&loc=Local", dbUser, dbPass, dbHost, dbName)
+}
