7616c6b9f225fd7abd611554f512d60a9891a256
augustine <augustino0890@gmail.com>
2021-07-11 00:09:20 +0900

    head-first/functions: update calculate perimeter

--- head-first/functions/perimeter.go
+++ head-first/functions/perimeter.go
@@ -1,13 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
-func calculatePerimeter(width float64, length float64) float64 {
+func calculatePerimeter(width float64, length float64) (float64, error) {
+	if width < 0 {
+		return 0, fmt.Errorf("a width of %.2f is invalid", width)
+	}
+
+	if length < 0 {
+		return 0, fmt.Errorf("a length of %.2f is invalid", length)
+	}
 	perimeter := (width + length) * 2
-	return perimeter
+	return perimeter, nil
 }
 
 func main() {
-	total := calculatePerimeter(8.2, 10) + calculatePerimeter(5, 5.2) + calculatePerimeter(6.2, 4.5)
-	fmt.Printf("You'll need %.2f meters of fencing\n", total)
+	p, err := calculatePerimeter(7.3, -12)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("You'll need %.2f meters of fencing\n", p)
 }
