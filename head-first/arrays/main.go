80b7a112d75b4667a599e2eae4f95c3febe53f53
augustine <augustino0890@gmail.com>
2021-07-14 18:20:49 +0900

    head-first: count accurrences

--- head-first/arrays/main.go
+++ head-first/arrays/main.go
@@ -4,6 +4,7 @@ import (
 	"arrays/datafile"
 	"fmt"
 	"log"
+	"reflect"
 )
 
 func declare() {
@@ -41,6 +42,24 @@ func weekday() {
 		fmt.Printf("%v %v\n", index, day)
 	}
 }
+
+func counting(numbers interface{}) {
+	arr := reflect.ValueOf(numbers)
+
+	// We'll count the number of times each number occurs
+	// within this array.
+	counts := make(map[int64]int)
+
+	for i := 0; i < arr.Len(); i++ {
+		val := arr.Index(i).Int()
+		counts[val]++
+	}
+	for number, count := range counts {
+		fmt.Printf("%v occurred %v\n", number, count)
+	}
+
+}
+
 func main() {
 	fmt.Println("Go arrays")
 	declare()
@@ -50,6 +69,19 @@ func main() {
 
 	fmt.Println("Go arrays weekday")
 	weekday()
+
+	fmt.Println("Go arrays counting")
+	numberCount := []int64{1, 0, 2, 0, 1, 0, 0, 1, 2}
+	counting(numberCount)
+
+	numbersCount := []int64{4, 2, 6, 7, 8, 0, 1, 8, 7, 8,
+		1, 5, 3, 2, 2, 1, 9, 6, 1, 0, 0, 0, 8, 4, 6,
+		2, 2, 4, 7, 9, 6, 5, 9, 0, 5, 1, 1, 5, 4, 7,
+		7, 9, 7, 8, 6, 3, 3, 3, 4, 8, 0, 4, 1, 1, 7,
+		9, 8, 8, 1, 2, 3, 6, 4, 9, 2, 5, 8, 6, 7, 7,
+		5, 4, 2, 9, 4, 4, 2, 2, 5, 5, 0, 0, 0, 9, 1,
+		9, 5, 8, 0, 1, 1, 0, 5, 3, 8, 6, 3, 4, 4, 9}
+	counting(numbersCount)
 
 	numbers, err := datafile.GetFloats("data.txt")
 	if err != nil {
