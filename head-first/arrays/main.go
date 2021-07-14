68a397a8fbdb7e490f92f1e267c57fc4785def96
augustine <augustino0890@gmail.com>
2021-07-14 17:17:42 +0900

    head-first: arrays

--- head-first/arrays/main.go
+++ head-first/arrays/main.go
@@ -6,7 +6,42 @@ import (
 	"log"
 )
 
+func declare() {
+	var numbers [3]int
+	numbers[0] = 43
+	numbers[1] = 21
+	numbers[2] = 17
+	fmt.Printf("%#v\n", numbers)
+
+	var letters = [3]string{"a", "b", "c"}
+	fmt.Printf("%#v\n", letters)
+
+	for i := 0; i < len(letters); i++ {
+		fmt.Printf("index %v: %v\n", i, letters[i])
+	}
+
+	var notes = [7]string{"do", "re", "mi", "fa", "so", "la", "si"}
+	for index, note := range notes {
+		fmt.Printf("index %v: %v\n", index, note)
+	}
+}
+
+func puzzle() {
+	numbers := [6]int{3, 16, -2, 10, 24, 14}
+	for index, number := range numbers {
+		if number >= 10 && number <= 20 {
+			fmt.Println(index, number)
+		}
+	}
+}
+
 func main() {
+	fmt.Println("Go arrays")
+	declare()
+
+	fmt.Println("Go arrays puzzle")
+	puzzle()
+
 	numbers, err := datafile.GetFloats("data.txt")
 	if err != nil {
 		log.Fatal(err)
