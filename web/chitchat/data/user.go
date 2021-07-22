cb69ff7a753ef66d75b8f78a80130d5561e1ed44
augustine <augustino0890@gmail.com>
2021-07-22 16:41:30 +0900

    go-web/chatchit: delete, update user and user test

--- web/chitchat/data/user.go
+++ web/chitchat/data/user.go
@@ -100,10 +100,69 @@ func (user *User) Create() (err error) {
 	return
 }
 
+// Delete user from database
+func (user *User) Delete() (err error) {
+	statement := "DELETE FROM users WHERE id = $1"
+	stmt, err := Db.Prepare(statement)
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(user.Id)
+	return
+}
+
+// Update user information in the database
+func (user *User) Update() (err error) {
+	statement := "UPDATE users SET name = $2, email = $3 WHERE id = $1"
+	stmt, err := Db.Prepare(statement)
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(user.Id, user.Name, user.Email)
+	return
+}
+
+// Get all users in the database and return it
+func Users() (users []User, err error) {
+	rows, err := Db.Query("SELECT id, uuid, name, email, password, created_at FROM users")
+	if err != nil {
+		return
+	}
+	for rows.Next() {
+		user := User{}
+		if err = rows.Scan(&user.Id, &user.Uuid, &user.Name, &user.Email, &user.Password, &user.CreateAt); err != nil {
+			return
+		}
+		users = append(users, user)
+	}
+	rows.Close()
+	return
+}
+
 // Get a single user given by email
 func UserByEmail(email string) (user User, err error) {
 	user = User{}
 	err = Db.QueryRow("SELECT id, uuid, name, email, password, created_at FROM users WHERE email = $1", email).Scan(
 		&user.Id, &user.Uuid, &user.Name, &user.Email, &user.Password, &user.CreateAt)
+	return
+}
+
+// Delete all users from database
+func UserDeleteAll() (err error) {
+	statement := "DELETE FROM users"
+	_, err = Db.Exec(statement)
+	return
+}
+
+// Get a single user given the UUID
+func UserByUUID(uuid string) (user User, err error) {
+	user = User{}
+	err = Db.QueryRow("SELECT id, uuid, name, email, password, created_at FROM users WHERE uuid = $1", uuid).Scan(
+		&user.Id, &user.Uuid, &user.Name, &user.Email, &user.Password, &user.CreateAt,
+	)
 	return
 }
