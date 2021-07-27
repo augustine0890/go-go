8fd58757ca3b4f34dad5f240f2ec9e58f33af47f
augustine <augustino0890@gmail.com>
2021-07-27 18:31:20 +0900

    gin: render XML responses

--- Projects/hello/main.go
+++ Projects/hello/main.go
@@ -1,13 +1,26 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/xml"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+type Person struct {
+	XMLName xml.Name `xml:"person"`
+	FirstName string `xml:"firstName,attr"`
+	LastName string `xml:"lastName,attr"`
+}
+
+func IndexHandler(c *gin.Context) {
+	c.XML(http.StatusOK, Person{
+		FirstName: "Augustine",
+		LastName: "Nguyen",
+	})
+}
 
 func main() {
 	router := gin.Default()
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "hello world",
-		})
-	})
+	router.GET("/", IndexHandler)
 	router.Run(":8080")
 }
