69faf9ae234d1223a994618adf08a8d9bfb7ecb5
augustine <augustino0890@gmail.com>
2021-09-15 18:30:15 +0900

    snippetbox: database connection

--- web/snippetbox/cmd/web/main.go
+++ web/snippetbox/cmd/web/main.go
@@ -1,21 +1,32 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/template/html"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 var (
 	port = flag.String("port", ":4000", "Port to listen")
+	dsn  = flag.String("dsn", "admin:admin@/snippetbox?parseTime=true", "MariaDB data source name")
 )
 
 func main() {
 	// Parse flags
 	flag.Parse()
+
+	// Connection pool
+	db, err := openDB(*dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 
 	// Initialize html tamplate engine
 	engine := html.New("./ui/html", ".html")
@@ -35,4 +46,16 @@ func main() {
 
 	log.Printf("Starting server on %s", *port)
 	log.Fatal(app.Listen(*port))
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
