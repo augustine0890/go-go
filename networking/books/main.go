13ad71a1575871e501bf28583a7db2e1a2e53e55
augustine <augustino0890@gmail.com>
2021-10-29 18:07:06 +0900

    rest-api: book and author models

--- networking/books/main.go
+++ networking/books/main.go
@@ -7,6 +7,18 @@ import (
 )
 
 // Models
+type Book struct {
+	ID string `json:"id"`
+	ISBN string `json:"isbn"`
+	Title string `json:"title"`
+	Author *Author `json:"author"`
+}
+
+type Author struct {
+	FirstName string `json:"first_name"`
+	LastName string `json:"last_name"`
+
+}
 
 func main() {
 	// Router
