56f93a9b2f3f37f55c5e11da380d08f4ccbee7fe
augustine <augustino0890@gmail.com>
2021-11-19 14:38:27 +0900

    banking: xml encoding

--- Projects/banking/main.go
+++ Projects/banking/main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"encoding/json"
+	"encoding/xml"
 	"fmt"
 	"log"
 	"net/http"
 )
 
 type Customer struct {
-	Name    string `json:"name"`
-	City    string `json:"city"`
-	Zipcode string `json:"zipcode"`
+	Name    string `json:"name" xml:"name"`
+	City    string `json:"city" xml:"city"`
+	Zipcode string `json:"zipcode" xml:"zipcode"`
 }
 
 func greetCustomer(w http.ResponseWriter, r *http.Request) {
@@ -23,8 +24,14 @@ func getAllCustomer(w http.ResponseWriter, r *http.Request) {
 		{"David", "Hanoi", "1063566"},
 		{"Juth", "London", "143436"},
 	}
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(customers)
+
+	if r.Header.Get("Content-Type") == "application/xml" {
+		w.Header().Set("Content-Type", "application/xml")
+		xml.NewEncoder(w).Encode(customers)
+	} else {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(customers)
+	}
 }
 
 func main() {
