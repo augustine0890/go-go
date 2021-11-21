699bb63c90228964ebdb40481d3e60416c51356c
augustine <augustino0890@gmail.com>
2021-11-21 20:46:11 +0900

    fifa: dispatch winners URL

--- web/fifa/handlers/handlers.go
+++ web/fifa/handlers/handlers.go
@@ -47,3 +47,15 @@ func AddNewWinner(res http.ResponseWriter, req *http.Request) {
 		res.WriteHeader(http.StatusCreated)
 	}
 }
+
+// WinnersHandler is the dispatcher for all /winner URL
+func WinnersHandler(res http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case http.MethodGet:
+		ListWinners(res, req)
+	case http.MethodPost:
+		AddNewWinner(res, req)
+	default:
+		res.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
