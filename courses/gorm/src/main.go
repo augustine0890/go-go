54ca77634d09dfc52db1b9c0e4bf5b061e000e41
augustine <augustino0890@gmail.com>
2021-11-21 22:50:38 +0900

    gorm: CRUD query records

--- courses/gorm/src/main.go
+++ courses/gorm/src/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"log"
+	"fmt"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -9,13 +9,46 @@ import (
 
 func main() {
 	dsn := "host=localhost user=postgres password=postgres dbname=database port=5432 sslmode=disable"
-	connection, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		panic(err.Error())
+		panic("Failed to connect to database")
 	}
-	db, err := connection.DB()
-	if err != nil {
-		log.Fatal(err.Error())
+
+	db.Migrator().DropTable(&User{})
+	db.AutoMigrate(&User{})
+
+	for _, user := range users {
+		db.Create(&user)
 	}
-	defer db.Close()
+	u := User{Username: "tmacmillan"}
+	// Querying
+	// db.First(&u)
+	// db.Last(&u)
+
+	// Updating
+	db.Where(&u).First(&u)
+	u.LastName = "Beeblebrox"
+	db.Save(&u)
+
+	user := User{}
+	db.Where(&u).First(&user)
+
+	// Deleting
+	db.Where(&User{Username: "mrobot"}).Delete(&User{})
+
+	fmt.Println(user)
+}
+
+type User struct {
+	ID        int
+	Username  string
+	FirstName string
+	LastName  string
+}
+
+var users []User = []User{
+	{Username: "august", FirstName: "Augustine", LastName: "Nguyen"},
+	{Username: "fperfact", FirstName: "Ford", LastName: "Prefect"},
+	{Username: "tmacmillan", FirstName: "Tricia", LastName: "MacMillan"},
+	{Username: "mrobot", FirstName: "Marvin", LastName: "Robot"},
 }
