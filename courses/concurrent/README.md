80c650391538db0ba9ae29b25118dbd6e5529a2b
augustine <augustino0890@gmail.com>
2021-11-22 11:08:43 +0900

    concurrent: create goroutines

--- courses/concurrent/README.md
+++ courses/concurrent/README.md
@@ -2,9 +2,17 @@
 
 ## Concurrency and parallelism
 - Single task: have a single task
-- Concurrency: have multiple tasks
+- Concurrency: have multiple tasks (Don't need at the same time)
 - Parallelism: execute multiple tasks simultaneously
-
+- Concurrency is a prerequisite for parallel execution
 ## Goroutines
+- Thread:
+  - Ability of OS to run the tasks
+  - Have own execution stack
+  - Fixed stack space (around 1MB)
+  - Managed by OS
 
-
+- Goroutines:
+  - Have own execution stack
+  - Variable stack space (starts @2KB)
+  - Manage by Go runtime
--- courses/concurrent/main.go
+++ courses/concurrent/main.go
@@ -12,18 +12,24 @@ var rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
 func main() {
 	for i := 0; i < 10; i++ {
 		id := rnd.Intn(10) + 1
-		if b, ok := queryCache(id); ok {
-			fmt.Println("from cache")
-			fmt.Println(b)
-			continue
-		}
-		if b, ok := queryDatabase(id); ok {
-			fmt.Println("from database")
-			cache[id] = b
-			fmt.Println(b)
-			continue
-		}
-		fmt.Printf("Book not found: %v", id)
+		go func(id int) {
+			if b, ok := queryCache(id); ok {
+				fmt.Println("from cache")
+				fmt.Println(b)
+			}
+		}(id)
+
+		go func(id int) {
+			if b, ok := queryDatabase(id); ok {
+				fmt.Println("from database")
+				cache[id] = b
+				fmt.Println(b)
+			}
+		}(id)
+		// This is waiting for goroutine to finish (complete). The program execution completes when the main function exits
+		// But we don't guarantee all of go routines to complete
+		time.Sleep(time.Microsecond * 150)
+		// fmt.Printf("Book not found with id: %v\n", id)
 	}
 }
 
