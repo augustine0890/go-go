38f93a319bc97adc114eb8259ed125f7b81c874c
augustine <augustino0890@gmail.com>
2022-01-04 17:31:51 +0900

    go-action: create goroutines with single logical processor

---
+++ go-in-action/concurrency/go-02.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+// wg is used to wait for the program to finish
+var wg sync.WaitGroup
+
+func main() {
+	// Allocate 1 logical memory processors for scheduler to used
+	// Change the runtime to allocate a logical processor for every available physical processor.
+	runtime.GOMAXPROCS(1)
+
+	wg.Add(2)
+
+	fmt.Println("Create Goroutines")
+
+	go printPrime("A")
+	go printPrime("B")
+
+	// Wait for the goroutines to finish
+	fmt.Println("Wait for the goroutines to finish")
+	wg.Wait()
+
+	fmt.Println("Terminating Program...")
+}
+
+// printPrime displays prime numbers for the first 5000 numbers.
+func printPrime(prefix string) {
+	// Schedule the call to Done to tell main we are done
+	defer wg.Done()
+
+next:
+	for outer := 2; outer < 5000; outer++ {
+		for inner := 2; inner < outer; inner++ {
+			if outer%inner == 0 {
+				continue next
+			}
+		}
+		fmt.Printf("%s:%d\n", prefix, outer)
+	}
+	fmt.Println("Completed", prefix)
+}
