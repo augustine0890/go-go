31b63c60d48ac8a1d965aca7adb029b7f2e8b9cf
augustine <augustino0890@gmail.com>
2022-01-11 12:42:11 +0900

    go-project: primary functionality of the logger

--- Projects/async-logging/aslog.go
+++ Projects/async-logging/aslog.go
@@ -2,9 +2,12 @@
 package aslog
 
 import (
+	"fmt"
 	"io"
 	"os"
+	"strings"
 	"sync"
+	"time"
 )
 
 // Aslog is a type that defines a logger. It can be used to write log messages synchronously (via the Write method)
@@ -25,6 +28,62 @@ func New(w io.Writer) *Alog {
 		w = os.Stdout
 	}
 	return &Alog{
-		dest: w,
+		dest:    w,
+		msgCh:   make(chan string),
+		errorCh: make(chan error),
+		m:       &sync.Mutex{},
 	}
+}
+
+// Start begins the message loop for the asynchronous logger. It should be initiated as a goroutine to prevent the caller from being blocked
+func (al Alog) Start() {
+	for {
+		msg := <-al.msgCh
+		go al.write(msg, nil)
+	}
+}
+
+func (al Alog) formatMessage(msg string) string {
+	if !strings.HasSuffix(msg, "\n") {
+		msg += "\n"
+	}
+	return fmt.Sprintf("[%v] - %v", time.Now().Format("2006-01-02 15:04:05"), msg)
+}
+
+func (al Alog) write(msg string, wg *sync.WaitGroup) {
+	al.m.Lock()
+	defer al.m.Unlock()
+	_, err := al.dest.Write([]byte(al.formatMessage(msg)))
+	if err != nil {
+		go func(err error) {
+			al.errorCh <- err
+		}(err)
+	}
+}
+
+func (al Alog) shutdown() {
+
+}
+
+// MessageChannel returns a channel that accepts messages that should be written to the log
+func (al Alog) MessageChannel() chan string {
+	return al.msgCh
+}
+
+// ErrorChannel returns a channel that will be pupulated when an error is raised during a write operation
+// This channel should always be monitored in some way to prevent deadlock goroutines from being Generated
+// when errors occur
+func (al Alog) ErrorChannel() chan error {
+	return al.errorCh
+}
+
+// Stop shuts down the logger. It will wait for all pending messages to be written and then return
+// The logger will no longer function after this method has been called.
+func (al Alog) Stop() {
+
+}
+
+// Write synchronously sends the mesaage to the log output
+func (al Alog) Write(msg string) (int, error) {
+	return al.dest.Write([]byte(al.formatMessage(msg)))
 }
--- Projects/async-logging/cmd/main.go
+++ Projects/async-logging/cmd/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"aslog"
+	"bufio"
 	"flag"
+	"fmt"
 	"io"
 	"log"
 	"os"
@@ -11,15 +14,63 @@ import (
 func main() {
 	out := flag.String("out", "stdout", "File name to use for log output. If stdout is provided, then output is written directly to the console.")
 	async := flag.Bool("async", false, "This flag determines if the logger should write asynchronously.")
+	flag.Parse()
 
 	var w io.Writer
-	var err error
+	// var err error
 	if strings.ToLower(*out) == "stdout" {
 		w = os.Stdout
 	} else {
-		w, err := os.Create(*out)
+		_, err := os.Create(*out)
 		if err != nil {
 			log.Fatal("Unable to open log file", err)
+		}
+	}
+
+	l := aslog.New(w)
+	go l.Start()
+
+	messageChan := l.MessageChannel()
+	errChan := l.ErrorChannel()
+
+	if errChan != nil {
+		go func(errChan <-chan error) {
+			err := <-errChan
+			l.Stop()
+			log.Fatalf("Error received from logger: %v\n", err)
+		}(errChan)
+	}
+
+	for {
+		reader := bufio.NewReader(os.Stdin)
+
+		fmt.Println("Please enter message to write to log or 'q' to quit.")
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Unable to read input from command line, please try again.", err)
+			continue
+		}
+
+		if strings.ToLower(input) == "q\n" || strings.ToLower(input) == "q\r\n" {
+			if wc, ok := w.(io.Closer); ok {
+				err := wc.Close()
+				if err != nil {
+					fmt.Println("Failed to close log file:", err)
+				}
+			}
+			l.Stop()
+			break
+		}
+
+		if *async {
+			if messageChan != nil {
+				messageChan <- input
+			}
+		} else {
+			_, err := l.Write(input)
+			if err != nil {
+				fmt.Println("Unable to write message out to log")
+			}
 		}
 	}
 }
