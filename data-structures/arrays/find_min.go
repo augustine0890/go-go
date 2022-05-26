b8a2d450add7fb239edb45ca4036f378033e98fd
augustine <augustino0890@gmail.com>
2022-05-26 17:00:35 +0900

    ds: find minimum value using sorting

--- data-structures/arrays/find_min.go
+++ data-structures/arrays/find_min.go
@@ -14,7 +14,50 @@ func findMin(arr []int) int {
 	return min
 }
 
+func mergeSort(arr []int) []int {
+	if len(arr) < 2 {
+		return arr
+	}
+	first := mergeSort(arr[:len(arr)/2])
+	second := mergeSort(arr[len(arr)/2:])
+	return merge(first, second)
+}
+
+func merge(first, second []int) []int {
+	final := []int{}
+	i, j := 0, 0
+	for i < len(first) && j < len(second) {
+		if first[i] < second[j] {
+			final = append(final, first[i])
+			i++
+		} else {
+			final = append(final, second[j])
+			j++
+		}
+	}
+
+	for ; i < len(first); i++ {
+		final = append(final, first[i])
+	}
+
+	for ; j < len(second); j++ {
+		final = append(final, second[j])
+	}
+
+	return final
+}
+
+// mergeSort is in O(nlogn) --> takes O(nlogn) time
+func findMin2(arr []int) int {
+	sorted := mergeSort(arr)
+	return sorted[0]
+}
+
 func main() {
 	fmt.Println(findMin([]int{9, 2, 3, 6}))
 	fmt.Println(findMin([]int{4, 2, 1, 5, 0}))
+
+	fmt.Println(findMin2([]int{9, 2, 3, 6}))
+	fmt.Println(findMin2([]int{4, 2, 1, 5, 0}))
+
 }
