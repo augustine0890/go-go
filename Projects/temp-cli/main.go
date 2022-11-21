e907357436146db2e364f0d9ecbfa499fe9cc3d8
augustine <augustino0890@gmail.com>
2022-11-21 12:46:18 +0900

    temp-cli: parse the answer

--- Projects/temp-cli/main.go
+++ Projects/temp-cli/main.go
@@ -43,7 +43,7 @@ func main() {
 			printError(errReadingInput)
 		}
 
-		if shouldConvertAgain != "Y" {
+		if strings.TrimSpace(strings.ToUpper(shouldConvertAgain)) != "Y" {
 			fmt.Println("Good bye!")
 			break
 		}
