b9e6b6ec63d54e66674cf177fe96fb783d87d6d4
augustine <augustino0890@gmail.com>
2022-12-01 15:35:48 +0900

    get the min value

--- courses/functions/main.go
+++ courses/functions/main.go
@@ -6,8 +6,23 @@ func foo(y *int) {
 	*y = *y + 1
 }
 
+func getMin(vals ...int) int {
+	var minVal int = vals[0]
+	for _, v := range vals {
+		if v < minVal {
+			minVal = v
+		}
+	}
+
+	return minVal
+}
+
 // Slices contain a pointer to the array
 func main() {
+	defer func() {
+		fmt.Println("Bye!")
+	}()
+
 	x := 2
 	foo(&x)
 	fmt.Println(x)
@@ -15,4 +30,9 @@ func main() {
 	nums := []int{15, 1, 4, 2, 8, 5, 11, 10, 3, 19, 16}
 	BubbleSort(nums)
 	fmt.Println(nums)
+
+	mV := getMin(nums...)
+	fmt.Println(mV)
+
+	fmt.Println("Hello")
 }
